97701c261b3a6b9754e8a4483b37a5b564e25495
Kevin McMahon <kevbot.mcmahon@gmail.com>
2021-09-05 17:00:38 -0400

    added some comments

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from scrape import create_transcript_files
-from preprocess import create_docterm_matrix
+# from scrape import create_transcript_files
+# from preprocess import create_docterm_matrix
 from similarity import compute_similarity
 import argparse
 
@@ -16,11 +16,12 @@ Potential future improvements
 # comment when vector files have been
 # create_docterm_matrix()
 
+# parse run line for title flag and prompt user for keywords/title
 parser = argparse.ArgumentParser(description='Comedy RecSys')
 parser.add_argument('-T', action='store_true', help='Add -T flag search by title instead of query')
 args = parser.parse_args()
-
 prompt = '\nEnter name of a special: ' if args.T else '\nEnter search query: '
 query = input(prompt)
 
+# compute results of query and display
 compute_similarity(query, args.T)
